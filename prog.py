89d6bdbb34042aaad3e3886f680caefd4d33ee87
David Scott <dave9860@gmail.com>
2024-05-22 15:29:29 -0600

    added an echo argument

--- prog.py
+++ prog.py
@@ -1,3 +1,10 @@
 import argparse
-parser = argparse.ArgumentParser()
+
+'''
+
 parser.parse_args()
+'''
+parser = argparse.ArgumentParser()
+parser.add_argument("echo")
+args = parser.parse_args()
+print(args.echo)
