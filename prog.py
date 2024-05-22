1e4ee3ca1c7640c0b3724957dcdd158c73531ff3
David Scott <dave9860@gmail.com>
2024-05-22 15:39:27 -0600

    Verbosity, woohoo!

--- prog.py
+++ prog.py
@@ -13,8 +13,16 @@ parser.add_argument("echo", help="echo the string you use here")
 args = parser.parse_args()
 print(args.echo)
 '''
-
+'''
+third iteration
 parser = argparse.ArgumentParser()
 parser.add_argument("square", help="display a square of a given number", type=int)
 args = parser.parse_args()
 print(args.square**2)
+'''
+parser = argparse.ArgumentParser()
+parser.add_argument("-v", "--verbose", help="increase output verbosity",
+                    action="store_true")
+args = parser.parse_args()
+if args.verbose:
+    print("verbosity turned on")
