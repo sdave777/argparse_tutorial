3c63002cbe1d64f6b542b60560e8b3a0ab5491d3
David Scott <dave9860@gmail.com>
2024-05-22 15:48:31 -0600

    I bet you didn't know you were running a file.

--- prog.py
+++ prog.py
@@ -27,8 +27,7 @@ parser.add_argument("-v", "--verbosity", action="count", default=0)
 args = parser.parse_args()
 answer = args.x**args.y
 if args.verbosity >= 2:
-    print(f"{args.x} to the power {args.y} equals {answer}")
-elif args.verbosity >= 1:
-    print(f"{args.x}^{args.y} == {answer}")
-else:
-    print(answer)
+    print(f"Running '{__file__}'")
+if args.verbosity >= 1:
+    print(f"{args.x}^{args.y} == ", end="")
+print(answer)
