9db072c589a08214c26dc9a4257afb68c22f225e
David Scott <dave9860@gmail.com>
2024-05-22 15:46:41 -0600

    It just got real

--- prog.py
+++ prog.py
@@ -21,17 +21,14 @@ args = parser.parse_args()
 print(args.square**2)
 '''
 parser = argparse.ArgumentParser()
-parser.add_argument("square", type=int,
-                    help="display a square of a given number")
-parser.add_argument("-v", "--verbosity", action="count",
-                    help="increase output verbosity")
+parser.add_argument("x", type=int, help="the base")
+parser.add_argument("y", type=int, help="the exponent")
+parser.add_argument("-v", "--verbosity", action="count", default=0)
 args = parser.parse_args()
-answer = args.square**2
-
-# bugfix: replace == with >=
+answer = args.x**args.y
 if args.verbosity >= 2:
-    print(f"the square of {args.square} equals {answer}")
+    print(f"{args.x} to the power {args.y} equals {answer}")
 elif args.verbosity >= 1:
-    print(f"{args.square}^2 == {answer}")
+    print(f"{args.x}^{args.y} == {answer}")
 else:
     print(answer)
