c8ebbae2e8b6dceb49330aaa36f4bcd97421b60e
David Scott <dave9860@gmail.com>
2024-05-22 15:33:33 -0600

    Now we're doing math

--- prog.py
+++ prog.py
@@ -1,10 +1,20 @@
 import argparse
 
 '''
-
+first iteration
+parser = argparse.ArgumentParser()
 parser.parse_args()
 '''
+
+'''
+second iteration
 parser = argparse.ArgumentParser()
-parser.add_argument("echo")
+parser.add_argument("echo", help="echo the string you use here")
 args = parser.parse_args()
 print(args.echo)
+'''
+
+parser = argparse.ArgumentParser()
+parser.add_argument("square", help="display a square of a given number", type=int)
+args = parser.parse_args()
+print(args.square**2)
